177577c0440291d200942411c524b84c905f06df
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-11-30 20:49:46 -0600

    completed creating new games

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { GameList } from "./game/gamelist.js"
 import { GameProvider } from "./game/gameprovider.js"
+import { GameForm } from "./game/gameform.js"
 import { EventList } from "./event/eventlist.js"
 import { EventProvider } from "./event/eventprovider.js"
 
@@ -12,15 +13,17 @@ export const ApplicationViews = () => {
             lineHeight: "1.75rem"
         }}> 
             <GameProvider>
-                <Route exact path="/">
-                    <GameList />
-                </Route>
+                <Route exact path="/" render={ props => <GameList {...props}/>} />
+                    
+                <Route exact path="/games/new" render={props => <GameForm {...props} />} />
+
+                <EventProvider>
+                    <Route exact path="/events">
+                        <EventList />
+                    </Route>
+
+                </EventProvider>
             </GameProvider>
-            <EventProvider>
-                <Route exact path="/events">
-                    <EventList />
-                 </Route>
-            </EventProvider>
         </main>
     </>
 }
