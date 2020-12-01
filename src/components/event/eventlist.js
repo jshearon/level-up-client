c778aba7226b7a4c814a8b753f03e25f25826743
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-11-30 23:04:09 -0600

    create new events complete

--- src/components/event/eventlist.js
+++ src/components/event/eventlist.js
@@ -12,6 +12,11 @@ export const EventList = (props) => {
         <article className="events">
             <header className="events__header">
                 <h1>Level Up Game Events</h1>
+                <button className="btn btn-2 btn-sep icon-create"
+                onClick={() => {
+                    props.history.push({ pathname: "/events/new" })
+                }}
+            >Create New Event</button>
             </header>
             {
                 events.map(event => {
